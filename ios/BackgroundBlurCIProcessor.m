68f59adb9be6c066dd991d8f332c850d1c12a609
Milosz Filimowski <milosz.filimowski@swmansion.com>
2026-03-05 13:47:09 +0100

    remove warmup

--- ios/BackgroundBlurCIProcessor.m
+++ ios/BackgroundBlurCIProcessor.m
@@ -46,83 +46,10 @@
             self->_segmentationRequest.qualityLevel = VNGeneratePersonSegmentationRequestQualityLevelBalanced;
             self->_segmentationRequest.outputPixelFormat = kCVPixelFormatType_OneComponent8;
             self->_sequenceHandler = [[VNSequenceRequestHandler alloc] init];
-
-            [self warmUpVisionModel];
-            [self warmUpCIFilters];
         }
 
         self->_ready = YES;
     });
-}
-
-- (void)warmUpVisionModel API_AVAILABLE(ios(15.0)) {
-    size_t warmUpWidth = 64;
-    size_t warmUpHeight = 64;
-    NSDictionary *attrs = @{
-        (id)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange),
-        (id)kCVPixelBufferWidthKey: @(warmUpWidth),
-        (id)kCVPixelBufferHeightKey: @(warmUpHeight),
-        (id)kCVPixelBufferIOSurfacePropertiesKey: @{},
-    };
-
-    CVPixelBufferRef dummyBuffer = NULL;
-    CVReturn result = CVPixelBufferCreate(
-        kCFAllocatorDefault, warmUpWidth, warmUpHeight,
-        kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
-        (__bridge CFDictionaryRef)attrs, &dummyBuffer);
-
-    if (result != kCVReturnSuccess || !dummyBuffer) return;
-
-    CVPixelBufferLockBaseAddress(dummyBuffer, 0);
-    for (size_t plane = 0; plane < CVPixelBufferGetPlaneCount(dummyBuffer); plane++) {
-        void *base = CVPixelBufferGetBaseAddressOfPlane(dummyBuffer, plane);
-        size_t h = CVPixelBufferGetHeightOfPlane(dummyBuffer, plane);
-        size_t bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(dummyBuffer, plane);
-        memset(base, 0, h * bytesPerRow);
-    }
-    CVPixelBufferUnlockBaseAddress(dummyBuffer, 0);
-
-    NSError *error = nil;
-    [_sequenceHandler performRequests:@[_segmentationRequest]
-                      onCVPixelBuffer:dummyBuffer
-                          orientation:kCGImagePropertyOrientationUp
-                                error:&error];
-
-    CVPixelBufferRelease(dummyBuffer);
-}
-
-- (void)warmUpCIFilters API_AVAILABLE(ios(15.0)) {
-    CIImage *dummy = [[CIImage imageWithColor:[CIColor colorWithRed:0 green:0 blue:0]]
-                      imageByCroppingToRect:CGRectMake(0, 0, 64, 64)];
-
-    CIFilter *blurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
-    [blurFilter setValue:[dummy imageByClampingToExtent] forKey:kCIInputImageKey];
-    [blurFilter setValue:@(_blurRadius) forKey:kCIInputRadiusKey];
-    CIImage *blurred = [blurFilter.outputImage imageByCroppingToRect:dummy.extent];
-
-    CIFilter *blendFilter = [CIFilter filterWithName:@"CIBlendWithMask"];
-    [blendFilter setValue:dummy forKey:kCIInputImageKey];
-    [blendFilter setValue:blurred forKey:kCIInputBackgroundImageKey];
-    [blendFilter setValue:dummy forKey:kCIInputMaskImageKey];
-    CIImage *result = blendFilter.outputImage;
-
-    NSDictionary *attrs = @{
-        (id)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_32BGRA),
-        (id)kCVPixelBufferWidthKey: @(64),
-        (id)kCVPixelBufferHeightKey: @(64),
-        (id)kCVPixelBufferIOSurfacePropertiesKey: @{},
-        (id)kCVPixelBufferMetalCompatibilityKey: @YES,
-    };
-
-    CVPixelBufferRef warmupBuffer = NULL;
-    CVPixelBufferCreate(kCFAllocatorDefault, 64, 64, kCVPixelFormatType_32BGRA,
-                        (__bridge CFDictionaryRef)attrs, &warmupBuffer);
-    if (warmupBuffer) {
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        [_context render:result toCVPixelBuffer:warmupBuffer bounds:dummy.extent colorSpace:colorSpace];
-        CGColorSpaceRelease(colorSpace);
-        CVPixelBufferRelease(warmupBuffer);
-    }
 }
 
 #pragma mark - VideoFrameProcessorDelegate
