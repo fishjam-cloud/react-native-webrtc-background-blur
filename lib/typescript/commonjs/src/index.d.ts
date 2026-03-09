96a217911bca381e5ea7be7cfde6bb4b29401fe9
Paweł Gadomski <xpfk.098@gmail.com>
2026-03-09 16:19:16 +0100

    Remove lib from tracking

--- lib/typescript/commonjs/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { useBackgroundBlur } from './useBackgroundBlur';
-export type { Spec as BackgroundBlurSpec } from './NativeBackgroundBlur';
-export { default as NativeBackgroundBlur } from './NativeBackgroundBlur';
-export { initializeBackgroundBlur, deinitializeBackgroundBlur, } from './BackgroundBlur';
-//# sourceMappingURL=index.d.ts.map
