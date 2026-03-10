f414370a509a3bf9b53727a3e0d4d5469e49dd6a
Milosz Filimowski <milosz.filimowski@swmansion.com>
2026-03-10 14:49:05 +0100

    fix spec folder

--- src/NativeBackgroundBlur.ts
+++ src/NativeBackgroundBlur.ts
@@ -1,11 +1,2 @@
-import type { TurboModule } from "react-native";
-import { TurboModuleRegistry } from "react-native";
-
-export interface Spec extends TurboModule {
-  initialize(): void;
-  deinitialize(): void;
-  setBlurRadius(radius: number): void;
-  isAvailable(): boolean;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>("BackgroundBlur");
+export type { Spec } from "../spec/NativeBackgroundBlur";
+export { default } from "../spec/NativeBackgroundBlur";
